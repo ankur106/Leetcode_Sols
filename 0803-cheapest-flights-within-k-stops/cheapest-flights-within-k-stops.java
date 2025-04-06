f510446ed17d1fa4ed0432e34be5b8be67611a4d
Ankur Patel <ankurnpatel106@gmail.com>
2025-04-06 11:40:32 -0700

    Time: 8 ms (35.39%) Space: 44.8 MB (20.50%)

---
+++ 0803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -0,0 +1,43 @@
+
+
+class Solution {
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        Map<Integer, List<int[]>> adjList = new HashMap<>();
+
+        for (int[] flight : flights) {
+            adjList.computeIfAbsent(flight[0], x -> new ArrayList<>()).add(new int[]{flight[1], flight[2]});
+        }
+
+        // minHeap: [cost, currentNode, stopsUsed]
+        Queue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(a[0], b[0]));
+        pq.offer(new int[]{0, src, 0});
+
+        // visited map to avoid reprocessing worse costs with same or more stops
+        Map<Integer, Integer> visited = new HashMap<>();
+
+        while (!pq.isEmpty()) {
+            int[] curr = pq.poll();
+            int cost = curr[0];
+            int node = curr[1];
+            int stops = curr[2];
+
+            if (node == dst) return cost;
+            if (stops > k) continue;
+
+            if (visited.containsKey(node) && visited.get(node) <= stops) {
+                continue;
+            }
+            visited.put(node, stops);
+
+            if (adjList.containsKey(node)) {
+                for (int[] neighbor : adjList.get(node)) {
+                    int nextNode = neighbor[0];
+                    int price = neighbor[1];
+                    pq.offer(new int[]{cost + price, nextNode, stops + 1});
+                }
+            }
+        }
+
+        return -1;
+    }
+}
