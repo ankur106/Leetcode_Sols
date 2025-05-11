4f09e10e1db5a7d2fe3eb6271d55e3520bb05ff5
Ankur Patel <ankurnpatel106@gmail.com>
2025-05-11 10:28:51 -0700

    Time: 0 ms (100.00%) Space: 42.1 MB (94.44%)

---
+++ 1293-three-consecutive-odds/three-consecutive-odds.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+
+        int len = arr.length;
+
+        for(int i=0; i < len -2; ++i){
+            if(arr[i] % 2 !=0 &&  arr[i +1] % 2 !=0 && arr[i+2] % 2 !=0) return true;
+
+        }
+
+
+        return false;
+        
+    }
+}
