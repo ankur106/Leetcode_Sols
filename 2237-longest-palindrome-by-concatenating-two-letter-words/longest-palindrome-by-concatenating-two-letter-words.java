0cf41eec5eff564af49e488bcb0a88d257f5a5cd
Ankur Patel <ankurnpatel106@gmail.com>
2025-05-25 10:43:35 -0700

    Time: 14 ms (92.28%) Space: 60.9 MB (10.57%)

--- 2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.java
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.java
@@ -1,44 +1,16 @@
-import java.util.HashMap;
-
 class Solution {
     public int longestPalindrome(String[] words) {
-
-        HashMap<String, Integer> map = new HashMap<>();
-
-        // Count frequency of each word
-        for (String word : words) {
-            map.put(word, map.getOrDefault(word, 0) + 1);
-        }
-
-        int ans = 0;
-        boolean hasMiddle = false;
-
-        for (String word : map.keySet()) {
-            int count = map.get(word);
-            if (count == 0) continue;
-
-            String revWord = new StringBuilder(word).reverse().toString();
-
-            if (word.charAt(0) == word.charAt(1)) {
-                int pairs = count / 2;
-                ans += pairs * 4;
-                map.put(word, count % 2);
-
-                if (!hasMiddle && map.get(word) > 0) {
-                    ans += 2;
-                    hasMiddle = true;
-                    map.put(word, map.get(word) - 1);
-                }
-
-            } else if (map.containsKey(revWord)) {
-                int revCount = map.get(revWord);
-                int pairs = Math.min(count, revCount);
-                ans += pairs * 4;
-                map.put(word, count - pairs);
-                map.put(revWord, revCount - pairs);
+        int ans=0;
+        int arr[][]=new int[26][26];
+        for(String s:words){
+            int a=s.charAt(0)-'a',b=s.charAt(1)-'a';
+            if(arr[b][a]!=0){
+                arr[b][a]--;
+                ans+=4;
             }
+            else arr[a][b]++;
         }
-
+        for(int i=0;i<26;i++) if(arr[i][i]!=0){ans+=2; break;}
         return ans;
     }
 }
