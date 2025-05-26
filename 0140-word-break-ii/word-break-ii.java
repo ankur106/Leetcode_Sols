b74df0aca18eae97f11c850ed1ba926f3b766271
Ankur Patel <ankurnpatel106@gmail.com>
2025-05-25 17:58:31 -0700

    Time: 1 ms (93.94%) Space: 41.5 MB (95.09%)

--- 0140-word-break-ii/word-break-ii.java
+++ 0140-word-break-ii/word-break-ii.java
@@ -1,40 +1,75 @@
+// class Solution {
+//     public List<String> wordBreak(String s, List<String> wordDict) {
+//         Map<Integer, List<String>> map = new HashMap<>();
+
+
+//         Set<String>  st = new HashSet<>(wordDict);
+
+
+//         solve(s, map, st, 0);
+
+//         return map.get(0);
+        
+//     }
+
+
+//     private void solve(String s,  Map<Integer, List<String>> map ,  Set<String>  st, int index){
+        
+//         map.put(index, new ArrayList<String>());
+
+//         List<String> curr = map.get(index);
+//         for(int i = index; i < s.length(); ++i){
+//             String sub = s.substring(index, i+1 );
+
+//             if(!st.contains(sub))   continue;
+            
+//             if(i + 1 == s.length()){ 
+//                 curr.add(sub);
+//                 break;    
+//             }
+
+//             if(!map.containsKey(i+1))solve(s, map, st, i+1);
+
+//             for(String ss : map.get(i+1)){
+//                 curr.add(sub + " " + ss);
+//             }
+
+
+//         }
+//     }
+// }
+
+
+
 class Solution {
     public List<String> wordBreak(String s, List<String> wordDict) {
-        Map<Integer, List<String>> map = new HashMap<>();
-
-
-        Set<String>  st = new HashSet<>(wordDict);
-
-
-        solve(s, map, st, 0);
-
-        return map.get(0);
-        
+        Set<String> wordSet = new HashSet<>(wordDict);
+        Map<Integer, List<String>> memo = new HashMap<>();
+        return dfs(s, 0, wordSet, memo);
     }
 
+    private List<String> dfs(String s, int index, Set<String> wordSet, Map<Integer, List<String>> memo) {
+        if (memo.containsKey(index)) return memo.get(index);
 
-    private void solve(String s,  Map<Integer, List<String>> map ,  Set<String>  st, int index){
-        
-        map.put(index, new ArrayList<String>());
+        List<String> result = new ArrayList<>();
 
-        List<String> curr = map.get(index);
-        for(int i = index; i < s.length(); ++i){
-            String sub = s.substring(index, i+1 );
-
-            if(!st.contains(sub))   continue;
-            
-            if(i + 1 == s.length()){ 
-                curr.add(sub);
-                break;    
-            }
-
-            if(!map.containsKey(i+1))solve(s, map, st, i+1);
-
-            for(String ss : map.get(i+1)){
-                curr.add(sub + " " + ss);
-            }
-
-
+        if (index == s.length()) {
+            result.add(""); // base case: empty string to concatenate
+            return result;
         }
+
+        for (int end = index + 1; end <= s.length(); end++) {
+            String word = s.substring(index, end);
+
+            if (!wordSet.contains(word)) continue;
+
+            List<String> subList = dfs(s, end, wordSet, memo);
+            for (String sub : subList) {
+                result.add(word + (sub.isEmpty() ? "" : " " + sub));
+            }
+        }
+
+        memo.put(index, result);
+        return result;
     }
 }
