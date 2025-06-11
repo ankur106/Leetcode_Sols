0e82951e6301e46070b03003bedf5a2cb63f4d27
Ankur Patel <ankurnpatel106@gmail.com>
2025-06-10 21:39:00 -0700

    Time: 4 ms (30.81%) Space: 45.1 MB (7.58%)

---
+++ 2468-valid-palindrome-iv/valid-palindrome-iv.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean makePalindrome(String s) {
+        int len = s.length();
+        int low = 0, high = s.length() - 1;
+
+        int change = 0 ;
+
+        while(low <= high){
+            if(s.charAt(low) != s.charAt(high)) change++;
+
+            low++;
+            high--;
+        }
+
+        return change <=2;
+        
+    }
+}
