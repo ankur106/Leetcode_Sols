bccfd1d0c61c62f23c633419ad2f66e9a245df83
Ankur Patel <ankurnpatel106@gmail.com>
2025-06-14 09:53:10 -0700

    Time: 10 ms (28.99%) Space: 55.3 MB (75.40%)

---
+++ 1428-jump-game-iii/jump-game-iii.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean canReach(int[] arr, int start) {
+        int n = arr.length;
+
+        Queue<Integer> q = new LinkedList<>();
+        q.add(start);
+
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            if (arr[node] == 0) {
+                return true;
+            }
+            if (arr[node] < 0) {
+                continue;
+            }
+
+            if (node + arr[node] < n) {
+                q.offer(node + arr[node]);
+            }
+            if (node - arr[node] >= 0) {
+                q.offer(node - arr[node]);
+            }
+            arr[node] = -arr[node];
+        }
+        return false;
+    }
+
+}
