1c57dfa56e2195e2ae83ac24538de1d6fac7f0f2
Ankur Patel <ankurnpatel106@gmail.com>
2025-06-21 23:42:09 -0700

    Time: 0 ms (100.00%) Space: 56.2 MB (59.58%)

--- 0882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
+++ 0882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
@@ -1,12 +1,12 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
         int l = 0, r = arr.length - 1, mid;
-        while (l < r) {
+        while (l <= r) {
             mid = (l + r) / 2;
             if (arr[mid] < arr[mid + 1])
                 l = mid + 1;
             else
-                r = mid;
+                r = mid -1;
         }
         return l;
     }
