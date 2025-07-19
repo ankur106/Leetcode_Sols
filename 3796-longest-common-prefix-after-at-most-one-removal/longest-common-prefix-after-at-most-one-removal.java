2026ecea353b088be26b93a8ac131a3e5d0bf827
Ankur Patel <ankurnpatel106@gmail.com>
2025-07-19 16:34:03 -0700

    Time: 7 ms (86.67%) Space: 46 MB (40.00%)

--- 3796-longest-common-prefix-after-at-most-one-removal/longest-common-prefix-after-at-most-one-removal.java
+++ 3796-longest-common-prefix-after-at-most-one-removal/longest-common-prefix-after-at-most-one-removal.java
@@ -1,34 +1,54 @@
 class Solution {
-    Map<String, Integer> memo = new HashMap<>();
-
     public int longestCommonPrefix(String s, String t) {
-        return dfs(s, t, 0, 0, 0);
-    }
-
-    private int dfs(String s, String t, int i, int j, int removed) {
-        if (i >= s.length() || j >= t.length()) return 0;
-
-        String key = i + "," + j + "," + removed;
-        if (memo.containsKey(key)) return memo.get(key);
-
-        int res = 0;
-
-        if (s.charAt(i) == t.charAt(j)) {
-            int take = 1 + dfs(s, t, i + 1, j + 1, removed);
-            int skip = 0;
-            if (removed == 0) {
-                skip = dfs(s, t, i + 1, j, 1);
-            }
-            res = Math.max(take, skip);
-        } else {
-            if (removed == 0) {
-                res = dfs(s, t, i + 1, j, 1);
+        int p1 = 0, p2 = 0;
+        int atmost = 0;
+        while (p1 < s.length() && p2 < t.length()) {
+            if (s.charAt(p1) == t.charAt(p2)) {
+                p1++; p2++;
+            } else if (atmost > 0) {
+                break;
             } else {
-                res = 0; 
+                p1++;
+                atmost++;
             }
         }
 
-        memo.put(key, res);
-        return res;
+        return p2;
     }
 }
+
+
+// class Solution {
+//     Map<String, Integer> memo = new HashMap<>();
+
+//     public int longestCommonPrefix(String s, String t) {
+//         return dfs(s, t, 0, 0, 0);
+//     }
+
+//     private int dfs(String s, String t, int i, int j, int removed) {
+//         if (i >= s.length() || j >= t.length()) return 0;
+
+//         String key = i + "," + j + "," + removed;
+//         if (memo.containsKey(key)) return memo.get(key);
+
+//         int res = 0;
+
+//         if (s.charAt(i) == t.charAt(j)) {
+//             int take = 1 + dfs(s, t, i + 1, j + 1, removed);
+//             int skip = 0;
+//             if (removed == 0) {
+//                 skip = dfs(s, t, i + 1, j, 1);
+//             }
+//             res = Math.max(take, skip);
+//         } else {
+//             if (removed == 0) {
+//                 res = dfs(s, t, i + 1, j, 1);
+//             } else {
+//                 res = 0; 
+//             }
+//         }
+
+//         memo.put(key, res);
+//         return res;
+//     }
+// }
