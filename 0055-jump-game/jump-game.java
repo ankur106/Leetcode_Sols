7e1188b829142a5c867dfb8340dbfdd36eacc53d
Ankur Patel <ankurnpatel106@gmail.com>
2025-07-21 22:52:08 -0700

    Time: 3 ms (50.41%) Space: 45.6 MB (70.80%)

--- 0055-jump-game/jump-game.java
+++ 0055-jump-game/jump-game.java
@@ -1,11 +1,12 @@
 public class Solution {
     public boolean canJump(int[] nums) {
-        int lastPos = nums.length - 1;
-        for (int i = nums.length - 1; i >= 0; i--) {
-            if (i + nums[i] >= lastPos) {
-                lastPos = i;
+        int farthest = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > farthest) {
+                return false; // We can't reach this position
             }
+            farthest = Math.max(farthest, i + nums[i]);
         }
-        return lastPos == 0;
+        return true;
     }
 }
