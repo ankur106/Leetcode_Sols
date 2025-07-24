07533a7ab2fdabd3fa4ad7d6edb588f3331bbde3
Ankur Patel <ankurnpatel106@gmail.com>
2025-07-23 20:28:19 -0700

    Time: 108 ms (42.82%) Space: 43 MB (30.00%)

--- 0312-burst-balloons/burst-balloons.java
+++ 0312-burst-balloons/burst-balloons.java
@@ -2,31 +2,33 @@ class Solution {
     public int maxCoins(int[] nums) {
         int n = nums.length;
         int[] arr = new int[n + 2];
-
+        
         arr[0] = 1;
         arr[n + 1] = 1;
-
+        
+        // Copy original nums to arr with padding of 1 at both ends
         for (int i = 0; i < n; i++) {
             arr[i + 1] = nums[i];
         }
+        int[][] memo = new int[n + 2][n + 2];
 
-        int[][] dp = new int[n + 2][n + 2];
+        return solve(arr, 1, n, memo); 
+    }
 
-        // len is the window size from i to j (inclusive of arr[i+1] to arr[j-1])
-        for (int len = 1; len <= n; len++) {
-            for (int i = 1; i <= n - len + 1; i++) {
-                int j = i + len - 1;
+    private int solve(int[] nums, int i, int j, int[][] memo) {
+        if (i > j) return 0;
+        if (memo[i][j] != 0) return memo[i][j];
 
-                for (int k = i; k <= j; k++) {
-                    int coins = arr[k] * arr[i - 1] * arr[j + 1];
-                    coins += dp[i][k - 1]; // left subproblem
-                    coins += dp[k + 1][j]; // right subproblem
+        int max = 0;
 
-                    dp[i][j] = Math.max(dp[i][j], coins);
-                }
-            }
+        for (int k = i; k <= j; k++) {
+            int coins = nums[k] * nums[i - 1] * nums[j + 1];
+            coins += solve(nums, i, k - 1, memo); 
+            coins += solve(nums, k + 1, j, memo); 
+            max = Math.max(max, coins);
         }
 
-        return dp[1][n];
+        memo[i][j] = max;
+        return max;
     }
 }
