ab82d92c779d22e96a12207d3bb0e6b53e82a7ff
Ankur Patel <ankurnpatel106@gmail.com>
2025-07-23 17:42:55 -0700

    Time: 5 ms (75.60%) Space: 58.2 MB (22.40%)

--- 0011-container-with-most-water/container-with-most-water.java
+++ 0011-container-with-most-water/container-with-most-water.java
@@ -20,37 +20,3 @@ public class Solution {
 }
 
 
-// class Solution {
-//     public int maxArea(int[] height) {
-//         int n = height.length;
-//         int[] left = new int[n];
-//         int[] right = new int[n];
-
-//         Stack<Integer> st = new Stack<>();
-//         for (int i = 0; i < n; i++) {
-//             while (!st.isEmpty() && height[st.peek()] < height[i]) {
-//                 st.pop();
-//             }
-//             left[i] = st.isEmpty() ? 0 : st.peek();
-//             st.push(i);
-//         }
-
-//         st.clear();
-//         for (int i = n - 1; i >= 0; i--) {
-//             while (!st.isEmpty() && height[st.peek()] < height[i]) {
-//                 st.pop();
-//             }
-//             right[i] = st.isEmpty() ? n - 1 : st.peek();
-//             st.push(i);
-//         }
-
-//         int maxArea = 0;
-//         for (int i = 0; i < n; i++) {
-//             int width = right[i] - left[i];
-//             int area = width * height[i];
-//             maxArea = Math.max(maxArea, area);
-//         }
-
-//         return maxArea;
-//     }
-// }
