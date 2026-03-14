462823c56097bd6aa06e1d208d949023a816d914
Ankur Patel <ankurnpatel106@gmail.com>
2026-03-14 11:27:43 -0700

    Time: 33 ms (56.58%) Space: 67 MB (20.92%)

---
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+class Solution {
+    public int numSubseq(int[] nums, int target) {
+        int n = nums.length;
+        int mod = 1_000_000_007;
+
+        Arrays.sort(nums);
+        int[] pow = new int[n];
+        pow[0] = 1;
+        for (int i = 1; i < n; i++) {
+            pow[i] = (pow[i - 1] * 2) % mod;
+        }
+
+        int left = 0;
+        int right = n - 1;
+        int ans = 0;
+
+        while (left <= right) {
+            if (nums[left] + nums[right] <= target) {
+                ans = (ans + pow[right - left]) % mod;
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return ans;
+    }
+}
