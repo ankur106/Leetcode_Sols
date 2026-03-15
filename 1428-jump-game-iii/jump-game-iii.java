5a37ac35d8519b7eccb93d102ccb7d2a2389ac44
Ankur Patel <ankurnpatel106@gmail.com>
2026-03-14 17:50:13 -0700

    Time: 3 ms (58.13%) Space: 53.1 MB (27.14%)

--- 1428-jump-game-iii/jump-game-iii.java
+++ 1428-jump-game-iii/jump-game-iii.java
@@ -1,28 +1,19 @@
 class Solution {
     public boolean canReach(int[] arr, int start) {
-        int n = arr.length;
-
-        Queue<Integer> q = new LinkedList<>();
-        q.add(start);
-
-        while (!q.isEmpty()) {
-            int node = q.poll();
-            if (arr[node] == 0) {
-                return true;
-            }
-            if (arr[node] < 0) {
-                continue;
-            }
-
-            if (node + arr[node] < n) {
-                q.offer(node + arr[node]);
-            }
-            if (node - arr[node] >= 0) {
-                q.offer(node - arr[node]);
-            }
-            arr[node] = -arr[node];
-        }
-        return false;
+        boolean[] visited = new boolean[arr.length];
+        return dfs(arr, start, visited);
     }
 
+    private boolean dfs(int[] arr, int idx, boolean[] visited) {
+        if (idx < 0 || idx >= arr.length) return false;
+
+        if (visited[idx]) return false;
+
+        if (arr[idx] == 0) return true;
+
+        visited[idx] = true;
+
+        return dfs(arr, idx + arr[idx], visited) || 
+               dfs(arr, idx - arr[idx], visited);
+    }
 }
