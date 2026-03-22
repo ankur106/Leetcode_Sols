93d3ab8208fa53efda46d7aaceddb4277a5b43de
Ankur Patel <ankurnpatel106@gmail.com>
2026-03-22 08:04:22 -0700

    Time: 1 ms (91.60%) Space: 42.8 MB (44.67%)

---
+++ 0029-divide-two-integers/divide-two-integers.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int divide(int dividend, int divisor) {
+        // overflow case
+        if (dividend == Integer.MIN_VALUE && divisor == -1) {
+            return Integer.MAX_VALUE;
+        }
+
+        long a = Math.abs((long) dividend);
+        long b = Math.abs((long) divisor);
+
+        int ans = 0;
+
+        while (a >= b) {
+            long temp = b;
+            int count = 1;
+
+            while (a >= temp + temp) {
+                temp = temp + temp;
+                count = count + count;
+            }
+
+            a -= temp;
+            ans += count;
+        }
+
+        if ((dividend < 0) ^ (divisor < 0)) {
+            ans = -ans;
+        }
+
+        return ans;
+    }
+}
