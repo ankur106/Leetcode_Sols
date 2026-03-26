6d7ce01622e70aa27ddde5ddc170521fd3dbbdbd
Ankur Patel <ankurnpatel106@gmail.com>
2026-03-25 17:17:48 -0700

    Time: 26 ms (11.41%) Space: 123.2 MB (14.09%)

---
+++ 1622-max-value-of-equation/max-value-of-equation.java
@@ -0,0 +1,19 @@
+class Solution {
+        public int findMaxValueOfEquation(int[][] points, int k) {
+        Deque<Pair<Integer, Integer>> ms = new ArrayDeque<>();
+        int res = Integer.MIN_VALUE;
+        for (int[] point : points) {
+            while (!ms.isEmpty() && point[0] - ms.peekFirst().getValue() > k) {
+                ms.pollFirst();
+            }
+            if (!ms.isEmpty()) {
+                res = Math.max(res, ms.peekFirst().getKey() + point[0] + point[1]);
+            }
+            while (!ms.isEmpty() && point[1] - point[0] > ms.peekLast().getKey()) {
+                ms.pollLast();
+            }
+            ms.offerLast(new Pair<>(point[1] - point[0], point[0]));
+        }
+        return res;
+    }
+}
