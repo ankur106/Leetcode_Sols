fb8d93743495441919e196e133a18ea620f01def
Ankur Patel <ankurnpatel106@gmail.com>
2026-07-28 08:42:16 -0700

    Time: 12 ms (62.56%) Space: 62.4 MB (48.62%)

--- 0820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 0820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,37 +1,32 @@
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        Set<Integer> ans = new HashSet<>();
-        int n = graph.length;
-        boolean[] visited = new boolean[n];
-        boolean[] visiting = new boolean[n]; // track current DFS stack for cycle detection
+        int len = graph.length;
+        int[] traverse = new int[len]; // 0 -> not visited, 1 -> visiting, 2 -> visited
 
-        for (int i = 0; i < n; ++i) {
-            if (!visited[i]) {
-                dfs(graph, visited, visiting, ans, i);
+        List<Integer> safeStates = new ArrayList<>();
+        for(int i = 0; i < len; ++i){
+            if(traverse[i] == 0){
+                dfs(graph, safeStates, i, traverse);
             }
         }
-
-        ArrayList<Integer> result = new ArrayList<>(ans);
-        Collections.sort(result);
-        return result;
+        Collections.sort(safeStates);
+        return safeStates;
     }
 
-    private boolean dfs(int[][] graph, boolean[] visited, boolean[] visiting, Set<Integer> ans, int node) {
-        if (visiting[node]) return false; 
-        if (visited[node]) return ans.contains(node); 
 
-        visiting[node] = true;
-        visited[node] = true;
+    private boolean dfs(int[][] graph, List<Integer> safeStates, int node, int[] traverse){
+        if(traverse[node] == 2) return true;
+        if(traverse[node] == 1) return false; // cycle found
 
-        for (int neighbor : graph[node]) {
-            if (!dfs(graph, visited, visiting, ans, neighbor)) {
-                visiting[node] = false; 
+        traverse[node] = 1;
+        for(int ngb : graph[node]){
+            if(!dfs(graph, safeStates, ngb, traverse)){
                 return false;
             }
         }
 
-        visiting[node] = false;
-        ans.add(node); 
+        safeStates.add(node);
+        traverse[node] = 2;
         return true;
     }
 }
