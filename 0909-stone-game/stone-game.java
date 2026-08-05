632cfcafed08ecbc06485fbae1f74e24567c8a48
Ankur Patel <ankurnpatel106@gmail.com>
2026-08-05 11:08:16 -0700

    Time: 9 ms (19.00%) Space: 47.2 MB (12.32%)

--- 0909-stone-game/stone-game.java
+++ 0909-stone-game/stone-game.java
@@ -1,59 +1,21 @@
-import java.util.Arrays;
-
 class Solution {
+    private int[][] memo;
+    private boolean[][] seen;
+
     public boolean stoneGame(int[] piles) {
-        int numberOfPiles = piles.length;
-        int[][] memo = new int[numberOfPiles][numberOfPiles];
-
-        for (int[] row : memo) {
-            Arrays.fill(row, -1);
-        }
-
-        int totalStones = 0;
-        for (int stones : piles) {
-            totalStones += stones;
-        }
-
-        int alexScore = findMaximumScore(
-                piles,
-                0,
-                numberOfPiles - 1,
-                memo
-        );
-
-        int leeScore = totalStones - alexScore;
-        return alexScore > leeScore;
+        int n = piles.length;
+        memo = new int[n][n];
+        seen = new boolean[n][n];
+        return diff(piles, 0, n - 1) > 0;
     }
 
-    // Maximum score the current player can obtain from piles[start...end].
-    private int findMaximumScore(
-            int[] piles,
-            int start,
-            int end,
-            int[][] memo
-    ) {
-        if (start > end) {
-            return 0;
-        }
-
-        if (start == end) {
-            return piles[start];
-        }
-
-        if (memo[start][end] != -1) {
-            return memo[start][end];
-        }
-        int chooseLeft = piles[start] + Math.min(
-                findMaximumScore(piles, start + 2, end, memo),
-                findMaximumScore(piles, start + 1, end - 1, memo)
-        );
-
-        int chooseRight = piles[end] + Math.min(
-                findMaximumScore(piles, start + 1, end - 1, memo),
-                findMaximumScore(piles, start, end - 2, memo)
-        );
-
-        memo[start][end] = Math.max(chooseLeft, chooseRight);
-        return memo[start][end];
+    private int diff(int[] piles, int i, int j) {
+        if (i > j) return 0;
+        if (seen[i][j]) return memo[i][j];
+        int best = Math.max(piles[i] - diff(piles, i + 1, j),
+                            piles[j] - diff(piles, i, j - 1));
+        seen[i][j] = true;
+        memo[i][j] = best;
+        return best;
     }
 }
